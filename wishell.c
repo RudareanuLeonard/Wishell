9557022e28ff163b5f1d0b6027ab7598e0a839b0
Leo <93031391+RudareanuLeonard@users.noreply.github.com>
2026-02-21 19:48:37 +0200

    .
    .

--- wishell.c
+++ wishell.c
@@ -3,46 +3,65 @@
 #include <sys/types.h>
 
 int BUFFER_SIZE = 256;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-void tokenization(char str[BUFFER_SIZE]){
+#define BUFFER_SIZE 1024
 
-    /*
-    
-    ls -la /my/folder
-    so the command itself is "ls" and "-la" and /my/folder are arguemnts
-    
-    */
+void tokenization(char str[BUFFER_SIZE]) {
 
     printf("tokenization function\n");
-
     printf("str = %s\n", str);
 
-    char *p;
-    char *command = NULL; //stored as readonly and points to the beginning of the command
-    __uint8_t did_command = 0;
+    char *token;
+    char *argv[BUFFER_SIZE];
+    int argc = 0;
 
+    // Remove trailing newline (important if using fgets)
+    str[strcspn(str, "\n")] = '\0';
 
-    p = strtok(str, " ");
-    printf("a\n");
+    // Tokenize input
+    token = strtok(str, " ");
 
-    while(p != NULL){
-        // printf("str = %s\n", p); <- this causes segfalut
-        if(did_command == 0){
-            printf("\n did_command just got value 1 \n");
-            command = p; //address of p
-            
-            did_command = 1;
-            
-            //run fork to run command
-            pid_t new_fork = fork(); // create this to make a child process of the shell
-            execvp("echo", "asda");
-        
-        }
-        p = strtok(NULL, " ");
+    while (token != NULL && argc < BUFFER_SIZE - 1) {
+        argv[argc++] = token;
+        token = strtok(NULL, " ");
     }
 
+    // If no command entered
+    if (argc == 0) {
+        printf("No command entered\n");
+        return;
+    }
 
+    // NULL terminate argument list (REQUIRED for execvp)
+    argv[argc] = NULL;
 
+    printf("Executing command: %s\n", argv[0]);
+
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        perror("fork failed");
+        return;
+    }
+
+    if (pid == 0) {
+        // Child process
+        execvp(argv[0], argv);
+
+        // If execvp returns, it failed
+        perror("execvp failed");
+        exit(EXIT_FAILURE);
+    } 
+    else {
+        // Parent process waits for child
+        wait(NULL);
+    }
 }
 
 
